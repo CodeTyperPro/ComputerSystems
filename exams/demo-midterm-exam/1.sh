b0fcd21e5b9df988bd3e78e2150e5c7dd43bd998
Alfredo Martins <alfredomartins624@gmail.com>
2022-11-04 00:08:12 +0100

    CheckFileNotWorking.

--- exams/demo-midterm-exam/1.sh
+++ exams/demo-midterm-exam/1.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
 
+isNumber(){
+	data=$1
+	res=`echo $data | grep "^[+-]\?[0-9]\+\(\.[0-9]\+\)\?$"`
+	return $res
+}
+
 if test $# -ne 2
 then
 	echo "Missing arguments."
+	exit 1
 else
 	x=$1
 	y=$2
+
+	isNumber $x
+	#x=$?
+
+	isNumber $y
+	#y=$?
+
+	if [[ ! -z $x || ! -z $y ]]
+	then
+		echo "Please, insert numbers. (valid inputs)"
+		exit 1
+	fi
 	
 	res=1
 	for i in `seq 1  $y`
--- exams/demo-midterm-exam/3.sh
+++ exams/demo-midterm-exam/3.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
 FILE=$1
+OUTPUT="short.txt"
 
 if test $# -eq 0
 then
 	echo "Missing filename via command line."
+	exit 0
 elif test -f $FILE
 then
-	echo "File exists!"
-	echo "" > short.txt
+	echo "" > $OUTPUT
 	while read line
 	do
-		len=`echo $line | wc -c`
-	       	echo $len
+		len=${#line}
 		if test $len -lt 5
-		then 
-			echo $line >> short.txt
+		then
+			echo $line >> $OUTPUT
 		fi
 	done < $FILE
+	echo "Output of $OUTPUT file: "
+	cat $OUTPUT
 else 
-	echo "Sorry! The file was not found in our system."
+	echo "Sorry! File not found."
 fi
