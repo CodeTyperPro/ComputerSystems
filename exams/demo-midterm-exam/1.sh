870fb0a5e994945f5ef7e016e0ee2f0beec0b49f
Alfredo Martins <alfredomartins624@gmail.com>
2022-11-04 02:07:32 +0100

    Something weird is happening.

--- exams/demo-midterm-exam/1.sh
+++ exams/demo-midterm-exam/1.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 isNumber(){
-	data=$1
-	res=`echo $data | grep "^[+-]\?[0-9]\+\(\.[0-9]\+\)\?$"`
-	return $res
+	result=`echo "$1" | grep "^[+-]\?[0-9]\+\(\.[0-9]\+\)\?$"`
+	
+	if test ${#result} -eq 0
+	then
+		return 0
+	fi
+	return 1
 }
 
 if test $# -ne 2
@@ -11,23 +15,27 @@ then
 	echo "Missing arguments."
 	exit 1
 else
+	isNumber $1
+	x=$?
+	
+	isNumber $2
+	y=$?
+	
+	if test $x -eq 0
+	then
+		echo "Please, insert valid input(s)."
+		exit 0
+	elif test $y -eq 0
+	then
+		echo "Please, valid input(s)."
+		exit 0
+	fi
+
 	x=$1
 	y=$2
-
-	isNumber $x
-	#x=$?
-
-	isNumber $y
-	#y=$?
-
-	if [[ ! -z $x || ! -z $y ]]
-	then
-		echo "Please, insert numbers. (valid inputs)"
-		exit 1
-	fi
 	
 	res=1
-	for i in `seq 1  $y`
+	for i in `seq 0  $y`
 	do
 		echo -n $res " "
 		res=`expr $res \* $x`
--- exams/demo-midterm-exam/4.sh
+++ exams/demo-midterm-exam/4.sh
@@ -2,34 +2,49 @@
 
 FILE=$1
 
-max(){
-	x=$1
-	y=$2
-
-	if test $x -ge $y
-	then
-		return $x	
-	fi	
-
-	return $y
-}
-
 if $# -eq 0
 then
 	echo "Please, pass the file via command line."
 elif test -f $FILE
 then
+	TEMP="temp.dat"
+	if test -f $TEMP
+	then 
+		rm $TEMP
+	fi
+
+	max_number_=0
+	index_maximum_=0
+	index=0
 	while read line
 	do
 		res=0
 		for j in $line
 		do
-			max $res $j
-			res=$?
+			if test $j -gt $res
+			then
+				res=$j
+			fi
 		done
 		
-		echo $res	
+		echo $res >> $TEMP # Temporary file
+
+		if test $res -gt $max_number_
+		then
+			max_number_=$res
+			index_maximum_=$index
+		fi
+
+		index=`expr $index + 1`
 	done < $FILE
+
+	#Update FILE
+	index=0
+	while read line
+	do
+		#`sed '$index i/{&} $line/g' $FILE`
+		index=`expr $index + 1`
+	done < $TEMP
 else
 	echo "File not found."
 fi
