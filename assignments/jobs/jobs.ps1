e12f29360b25e60359fdf6f93247382e42914be6
Alfredo Martins <alfredomartins624@gmail.com>
2022-12-01 16:55:19 +0100

    Almost done.

--- assignments/jobs/jobs.ps1
+++ assignments/jobs/jobs.ps1
@@ -1,4 +1,5 @@
 ﻿$FILE=$args[0]
+$PSDefaultParameterValues['*:Encoding'] = 'UTF8'
 
 #Important scripts
 # Print the MAIN MENU
@@ -22,22 +23,26 @@ function printHeader(){
 function getWorkplacesNoViolentActs(){
     
     Write-Host ":: WORKPLACES WHERE NO VIOLENT ACTS HAVE BEEN REPORTED ::"
-    $text = Get-Content $FILE
+    $text = @()
+    $text += Get-Content $FILE
     $result = ""
+    [int]$id = 1
     foreach($line in $text){
         $array_string = @()
         $array_string += $line.split(",")
-        Write-Host "Length = " + $array_string.Lenght # To be fixed.
 
-        if($array_string.Lenght -ne 4){
-            Write-Host "??? $line ???"
+        if($array_string.Count -ne 4){
+            Write-Output "??? $line ???"
             printInvalidDataMessage
         }
 
-        $number_of_violent_act = $array_string[2]
+        [int]$number_of_violent_act = $array_string[2]
+
+        #Write-Host $number_of_violent_act
 
         if($number_of_violent_act -eq 0){
-            $result += $array_string[0] + " `n"
+            $result += ($id.ToString() + " => " + $array_string[0] + " `n")
+            $id += 1
         }
     }
 
@@ -49,11 +54,74 @@ function getWorkplacesNoViolentActs(){
 function getTotalNumberOfSecurity(){
     Write-Host ":: TOTAL NUMBER OF SECURITY ::"
 
+    $text = @()
+    $text += Get-Content $FILE
+    [int] $total = [int] 0
+    foreach($line in $text){
+        $array_string = @()
+        $array_string += $line.split(",")
+
+        if($array_string.Count -ne 4){
+            Write-Output "??? $line ???"
+            printInvalidDataMessage
+        }
+
+        [int] $number_of_guards = $array_string[3]
+        [int] $total += $number_of_guards
+    }
+    
+    Write-Host ""
+    Write-Host "Total number of security guards => " $total.ToString()
+    Write-Host ""
+
     closeOrNot
 }
 
 function getHigherNumberOfViolentIncidents(){
     Write-Host ":: WORKPLACES WITH HIGHEST NUMBE OF VIOLENT INCIDENT (<NAME>, <ADDRESS>) ::"
+
+    $text = @()
+    $text += Get-Content $FILE
+    [int] $maximum = [int] 0
+    foreach($line in $text){
+        $array_string = @()
+        $array_string += $line.split(",")
+
+        if($array_string.Count -ne 4){
+            Write-Output "??? $line ???"
+            printInvalidDataMessage
+        }
+
+        [int] $number_of_guards = $array_string[3]
+        [int] $maximum =[math]::max($maximum, $number_of_guards)
+    }
+    
+    Write-Host ""
+    Write-Host "Highest number => " $maximum.ToString()
+    Write-Host ""
+    
+    $text = @()
+    $text += Get-Content $FILE
+    $result = ""
+    [int]$id = 1
+    foreach($line in $text){
+        $array_string = @()
+        $array_string += $line.split(",")
+
+        if($array_string.Count -ne 4){
+            Write-Output "??? $line ???"
+            printInvalidDataMessage
+        }
+
+        [int]$number_of_guards = $array_string[3]
+
+        if($number_of_guards -eq $maximum){
+            $result += ($id.ToString() + " => " + $array_string[0] + " `n")
+            $id += 1
+        }
+    }
+
+    Write-Host $result
 
     closeOrNot
 }
