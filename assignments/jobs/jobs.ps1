30272e70d8f12870204fcc6e01627c9931a9f31f
Alfredo Martins <alfredomartins624@gmail.com>
2022-12-01 11:03:43 +0100

    Some bugs to be fixed.

--- assignments/jobs/jobs.ps1
+++ assignments/jobs/jobs.ps1
@@ -11,10 +11,10 @@ function printHeader(){
     Write-Host "=================================================="
     Write-Host ":: WELCOME TO JOB QUERY SYSTEM ::"
     Write-Host "OPTIONS:"
-        Write-Host "\t:: W - Workplaces where no violent acts have been reported ::"
-        Write-Host "\t:: C - Total number of security guards ::"
-        Write-Host "\t:: H - Workplaces with higher number of violent incidents ::"
-        Write-Host "\t:: E - Exit::"
+        Write-Host "`t:: W - Workplaces where no violent acts have been reported ::"
+        Write-Host "`t:: C - Total number of security guards ::"
+        Write-Host "`t:: H - Workplaces with higher number of violent incidents ::"
+        Write-Host "`t:: E - Exit::"
     
     askOption
 }
@@ -23,19 +23,25 @@ function getWorkplacesNoViolentActs(){
     
     Write-Host ":: WORKPLACES WHERE NO VIOLENT ACTS HAVE BEEN REPORTED ::"
     $text = Get-Content $FILE
+    $result = ""
     foreach($line in $text){
-        Write.Output $line
         $array_string = @()
         $array_string += $line.split(",")
+        Write-Host "Length = " + $array_string.Lenght # To be fixed.
 
         if($array_string.Lenght -ne 4){
+            Write-Host "??? $line ???"
             printInvalidDataMessage
         }
 
-        foreach($item in $array_string){
-            Write.Output $item " "
+        $number_of_violent_act = $array_string[2]
+
+        if($number_of_violent_act -eq 0){
+            $result += $array_string[0] + " `n"
         }
     }
+
+    Write-Host $result
 
     closeOrNot
 }
@@ -62,7 +68,7 @@ function dismiss(){
     Write-Host ""
     $now = Get-Date
     Write-Host "THANK YOU FOR USING MY SCRIPT :) $now"
-    Write-Host "Copyright� ComSys ELTE 2022, Alfredo Martins (Student) & Dr. Bakonyi Vikt�ria (Teacher)"
+    Write-Host "Copyright© ComSys ELTE 2022, Alfredo Martins (Student) & Dr. Bakonyi Viktória (Teacher)"
     Exit
 }
 
@@ -86,6 +92,7 @@ function askOption(){
 
     [char]$OPTION = Read-Host "Enter the option"
     echo ""
+    Clear-Host
     switch($OPTION){ 
         'W' {getWorkplacesNoViolentActs}
         'C' {getTotalNumberOfSecurity}
